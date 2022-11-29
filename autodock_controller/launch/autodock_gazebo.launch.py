e3ca9d3be576ab607655eac0867c95ad2821c122
Ting Chang <ting.chang@adlinktech.com>
2022-11-29 11:12:01 +0800

    Add open_rviz to autodock_gazebo.launch.py

--- autodock_controller/launch/autodock_gazebo.launch.py
+++ autodock_controller/launch/autodock_gazebo.launch.py
@@ -6,30 +6,53 @@ from launch.actions import (DeclareLaunchArgument, GroupAction,
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.conditions import IfCondition
 
-tag_launch_dir = os.path.join(get_package_share_directory('apriltag_ros'), 'launch')
-config = os.path.join(get_package_share_directory('apriltag_docking'), 'param', 'neuronbot.yaml') 
-
-image_topic = LaunchConfiguration('image_topic', default="image_raw")
-camera_name = LaunchConfiguration('camera_name', default="/camera_color_frame")
 
 def generate_launch_description():
 
-    bringup_cmd_group = GroupAction([
-    Node(
-        package='apriltag_docking',
-        executable='controller',
-        name='autodock_controller',
-        output='screen',
-        parameters = [config]),
+    tag_launch_dir = os.path.join(get_package_share_directory('apriltag_ros'), 'launch')
+    config = os.path.join(get_package_share_directory('apriltag_docking'), 'param', 'neuronbot.yaml') 
+    rviz_config_dir = os.path.join(get_package_share_directory('apriltag_docking'), 'rviz', 'apriltag.rviz')
     
-    IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(tag_launch_dir,'tag_gazebo.launch.py')),
-        launch_arguments={'image_topic': image_topic,
-                          'camera_name': camera_name}.items())
+    open_rviz = LaunchConfiguration('open_rviz', default='false')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    image_topic = LaunchConfiguration('image_topic', default="image_raw")
+    camera_name = LaunchConfiguration('camera_name', default="/camera_color_frame")
+
+    arg_open_rviz = DeclareLaunchArgument(
+        'open_rviz',
+        default_value='false',
+        description='Launch Rviz?')
+
+    arg_use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+            
+    bringup_cmd_group = GroupAction([
+        Node(
+            package='apriltag_docking',
+            executable='controller',
+            name='autodock_controller',
+            output='screen',
+            parameters = [config]),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            condition=IfCondition(LaunchConfiguration("open_rviz"))),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(tag_launch_dir,'tag_gazebo.launch.py')),
+            launch_arguments={'image_topic': image_topic,
+                              'camera_name': camera_name}.items())
     ])
 
     ld = LaunchDescription()
+    ld.add_action(arg_open_rviz)
+    ld.add_action(arg_use_sim_time)
     ld.add_action(bringup_cmd_group)
 
     return ld
